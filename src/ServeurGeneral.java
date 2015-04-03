80a9047318fdb8a64816fb2c8630a710d329bbde
Robin Lintingre <robin.lintingre@gmail.com>
2015-04-03 18:24:32 +0200

    Création classes Serveur

---
+++ src/ServeurGeneral.java
@@ -0,0 +1,9 @@
+import java.rmi.server.RemoteObject;
+
+/**
+ * Created by Robin on 03/04/2015.
+ */
+public class ServeurGeneral  {
+
+
+}
---
+++ src/ServeurGeneralImpl.java
@@ -0,0 +1,11 @@
+import java.rmi.server.RemoteObject;
+
+/**
+ * Created by Robin on 03/04/2015.
+ */
+public class ServeurGeneralImpl extends RemoteObject {
+
+    public ServeurGeneralImpl() {
+        super();
+    }
+}
