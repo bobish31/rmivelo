8920c5a16cb320eb46978229d9d33fb23baa5879
Romain <romain-menu@orange.fr>
2015-04-21 22:55:03 +0200

    Ajout de veloBDD. En cours création du package classBDD avec peut-être rajout d'attribut

--- src/bdd/bddClass/UtilisateurBDD.java
+++ src/bdd/bddClass/UtilisateurBDD.java
@@ -4,4 +4,12 @@ package bdd.bddClass;
  * Created by Menu on 20/04/2015.
  */
 public class UtilisateurBDD {
+
+    // Numero de l'abonne
+    private int numero;
+
+    // Code de l'abonne
+    private int code;
+
+    //
 }
--- src/bdd/bddClass/VeloBDD.java
+++ src/bdd/bddClass/VeloBDD.java
@@ -4,4 +4,44 @@ package bdd.bddClass;
  * Created by Menu on 20/04/2015.
  */
 public class VeloBDD {
+
+    private int identifiantVelo;
+    private boolean operationnel;
+    private boolean enCirculation;
+
+    public VeloBDD ()
+    {
+        // Constructeur par defaut
+    }
+
+    public VeloBDD (int id){
+        this.identifiantVelo = id;
+        operationnel = true;
+        enCirculation = false;
+    }
+
+    public int getIdentifiant () {
+        return identifiantVelo;
+    }
+
+    public void setIdentifiantVelo(int identifiantVelo) {
+        this.identifiantVelo = identifiantVelo;
+    }
+
+    public boolean isOperationnel() {
+        return operationnel;
+    }
+
+    public void setOperationnel(boolean operationnel) {
+        this.operationnel = operationnel;
+    }
+
+    public boolean isEnCirculation() {
+        return enCirculation;
+    }
+
+    public void setEnCirculation(boolean enCirculation) {
+        this.enCirculation = enCirculation;
+    }
+
 }
